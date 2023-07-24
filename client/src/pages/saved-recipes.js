21129f68c68a989b18ea28cf922c138acdd4b9a5
Shuxin Weng <shuxinweng@gmail.com>
2023-07-23 23:41:02 -0400

    added unsave button in savedRecipe

--- client/src/pages/saved-recipes.js
+++ client/src/pages/saved-recipes.js
@@ -20,6 +20,20 @@ export const SavedRecipes = () => {
     fetchSavedRecipes();
   }, []);
 
+  const unsaveRecipe = async (recipeID) => {
+    try {
+      const response = await axios.delete("http://localhost:3001/recipes", {
+        data: {
+          recipeID,
+          userID,
+        },
+      });
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
       <h1> Saved Recipes </h1>
@@ -28,6 +42,7 @@ export const SavedRecipes = () => {
           <li key={recipe._id}>
             <div>
               <h2>{recipe.name}</h2>
+              <button onClick={() => unsaveRecipe(recipe._id)}>Unsave</button>
             </div>
             <div className="instructions">
               <p>{recipe.instructions}</p>
